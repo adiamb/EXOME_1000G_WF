1c8611f9f5c165e19c1f63c1633923c3527be963
Aditya Ambati <ambati@stanford.edu>
2018-02-25 18:40:19 -0800

    Update 1KG_EXOME_TASK.sh

--- 1KG_EXOME_TASK.sh
+++ 1KG_EXOME_TASK.sh
@@ -14,19 +14,24 @@ cat > "$1"_EXOME_TASK.sh <<- EOF
 #SBATCH --account=mignot
 #SBATCH --nodes=1 
 #SBATCH --ntasks=32
-#SBATCH --depend=afterok:"$5"
+#SBATCH --depend=afterok:$5
 module load bwa/0.7.8
 module load java/8u66
 gatk="GenomeAnalysisTK.jar"
 reffasta="ucsc.hg19.fasta"
-#wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$1/sequence_read/$3
-#wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$1/sequence_read/$4
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     BWA ALIGNMENT IN PROGRESS <<<<<<<<<<<<<<<<<<<<<<<<<<"
 bwa mem -M -t 32 -R "$2" \$reffasta $3 $4 > $1_bwamem.sam
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     PICARD SORT SAM IN PROGRESS    <<<<<<<<<<<<<<<<<<<<<<<<<<"
 java -Djava.io.tmpdir=/local/scratch -jar picard.jar SortSam I=$1_bwamem.sam O=$1_sorted.bam SORT_ORDER=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=True
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     PICARD REORDER SAM IN PROGRESS    <<<<<<<<<<<<<<<<<<<<<<<<<<"
 java -Djava.io.tmpdir=/local/scratch -jar picard.jar ReorderSam I=$1_sorted.bam O=$1_SORTED_REORD.bam R=\$reffasta VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=True
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     PICARD MARKING DUPLICATES    <<<<<<<<<<<<<<<<<<<<<<<<<<"
 java -Djava.io.tmpdir=/local/scratch -jar picard.jar MarkDuplicates I=$1_SORTED_REORD.bam O=$1_SORTED_REORD_DEDUP.bam M=$1_metrics.txt REMOVE_DUPLICATES=TRUE VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=True
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     GATK BASE RECALIBARATION    <<<<<<<<<<<<<<<<<<<<<<<<<<"
 java -Djava.io.tmpdir=/local/scratch -jar \$gatk -T BaseRecalibrator -R \$reffasta -L Exome_Agilent_V4.bed -I $1_SORTED_REORD_DEDUP.bam -knownSites dbsnp_138.hg19.vcf -knownSites Mills_and_1000G_gold_standard.indels.hg19.sites.vcf -nct 32 -o $1_RECAL.table
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     GATK PRINTING READS POST BQSR    <<<<<<<<<<<<<<<<<<<<<<<<<<"
 java -Djava.io.tmpdir=/local/scratch -jar \$gatk -T PrintReads -R \$reffasta -I $1_SORTED_REORD_DEDUP.bam -BQSR $1_RECAL.table -nct 32 -o $1_RECAL_READS.bam
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>     GATK HALPOTYPE CALLER -G.VCFs    <<<<<<<<<<<<<<<<<<<<<<<<<<"
 java -Djava.io.tmpdir=/local/scratch -jar \$gatk -T HaplotypeCaller -R \$reffasta -L Exome_Agilent_V4.bed -I $1_RECAL_READS.bam --dbsnp dbsnp_138.hg19.vcf --emitRefConfidence GVCF -nct 32 -o $1.raw.snps.indels.g.vcf
 EOF
 sbatch "$1"_EXOME_TASK.sh
