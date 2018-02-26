8e50cda2d1ba55c00f1669d61f97e20a09927aa1
Aditya Ambati <ambati@stanford.edu>
2018-02-25 18:32:11 -0800

    Update WGET_FASTA.sh

--- WGET_FASTA.sh
+++ WGET_FASTA.sh
@@ -4,14 +4,14 @@
 #$3 - mate2
 touch "$1"_WGET_TASK.sh
 chmod 777 "$1"_WGET_TASK.sh
-cat > "$1"_EXOME_TASK.sh <<- EOF
+cat > "$1"_WGET_TASK.sh <<- EOF
 #!/bin/bash -l
 #SBATCH --job-name=$1_EXOME_TASK
 #SBATCH --mem-per-cpu=16000
 #SBATCH --time=120:00:00
 #SBATCH --account=mignot
 #SBATCH --nodes=1 
-#SBATCH --ntasks=32
-#SBATCH --t 
 #wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$1/sequence_read/$3
 #wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/$1/sequence_read/$4
+EOF
+sbatch "$1"_WGET_TASK.sh
