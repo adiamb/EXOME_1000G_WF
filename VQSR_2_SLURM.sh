4f772f0f091d56ad1ee1790765ba39ba1b889773
Aditya Ambati <ambati@stanford.edu>
2018-06-26 10:13:48 -0700

    Update VQSR_2_SLURM.sh
    escaped $gatk to \$gatk

--- VQSR_2_SLURM.sh
+++ VQSR_2_SLURM.sh
@@ -12,7 +12,7 @@ cat > VQSR_2_STEP.sh <<-EOF
 module load java/8u66
 module load R/3.4.1
 gatk="GenomeAnalysisTK.jar"
-java -Djava.io.tmpdir=/local/scratch -jar $gatk -T ApplyRecalibration \
+java -Djava.io.tmpdir=/local/scratch -jar \$gatk -T ApplyRecalibration \
 -R ucsc.hg19.fasta -input $2 \
 -mode SNP --ts_filter_level 99.0 -recalFile recalibrate_SNP_$2.recal -tranchesFile recalibrate_SNP_$2.tranches -o recalibrated_$2.vcf
 EOF
