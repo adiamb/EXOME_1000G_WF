0fb68be1d64bd6c79069922ce2488e48b3f5cd2e
Aditya Ambati <ambati@stanford.edu>
2018-06-26 10:14:07 -0700

    Update VQSR_3_SLURM.sh
    escaped $gatk to \$gatk

--- VQSR_3_SLURM.sh
+++ VQSR_3_SLURM.sh
@@ -12,7 +12,7 @@ cat > VQSR_3_STEP.sh <<-EOF
 module load java/8u66
 module load R/3.4.1
 gatk="GenomeAnalysisTK.jar"
-java -Djava.io.tmpdir=/local/scratch -jar $gatk -T VariantRecalibrator \
+java -Djava.io.tmpdir=/local/scratch -jar \$gatk -T VariantRecalibrator \
 -R ucsc.hg19.fasta -input recalibrated_$2.vcf \
 -resource:mills,known=false,training=true,truth=true,prior=12.0 Mills_and_1000G_gold_standard.indels.hg19.sites.vcf \
 -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 dbsnp_138.hg19.vcf \
