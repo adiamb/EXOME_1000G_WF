9b9e520f64d8ffc057bbaa4d20179cf96b4a044a
Aditya Ambati <ambati@stanford.edu>
2018-06-26 10:33:11 -0700

    Update VQSR_2_SLURM.sh
    line 16 added *.vcf

--- VQSR_2_SLURM.sh
+++ VQSR_2_SLURM.sh
@@ -13,6 +13,6 @@ module load java/8u66
 module load R/3.4.1
 gatk="GenomeAnalysisTK.jar"
 java -Djava.io.tmpdir=/local/scratch -jar \$gatk -T ApplyRecalibration \
--R ucsc.hg19.fasta -input $2 \
+-R ucsc.hg19.fasta -input $2.vcf \
 -mode SNP --ts_filter_level 99.0 -recalFile recalibrate_SNP_$2.recal -tranchesFile recalibrate_SNP_$2.tranches -o recalibrated_$2.vcf
 EOF
