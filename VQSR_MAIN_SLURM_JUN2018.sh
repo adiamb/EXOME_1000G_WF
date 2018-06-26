697de88ceb55cb74bb17c2196b37df4f84c26e59
Aditya Ambati <ambati@stanford.edu>
2018-06-26 09:59:59 -0700

    intial
    main command

---
+++ VQSR_MAIN_SLURM_JUN2018.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+./VQSR_1_SLURM.sh $1
+JOBID0=$(sbatch --export=VQSR_1_STEP.sh)
+if ! echo ${JOBID0} | grep -q "[1-9][0-9]*$"; then 
+   echo "Job(s) submission failed."
+   echo ${JOBID0}
+   exit 1
+else
+   job0=$(echo ${JOBID0} | grep -oh "[1-9][0-9]*$")
+fi
+
+echo " SNP RECALIBRATION COMPELTED NOW APPLYING RECALIBRATION MAKE SURE TO CHECK THE RSCRIPTS TO GENERATE PLOTS"
+
+
+./VQSR_2_SLURM.sh $job0 $1
+JOBID1=$(sbatch --export=VQSR_2_STEP.sh)
+if ! echo ${JOBID1} | grep -q "[1-9][0-9]*$"; then 
+   echo "Job(s) submission failed."
+   echo ${JOBID1}
+   exit 1
+else
+   job1=$(echo ${JOBID1} | grep -oh "[1-9][0-9]*$")
+fi
+echo "VQSR of SNPS in progress "
+
+
+./VQSR_3_SLURM.sh $job1 $1
+JOBID2=$(sbatch --export=VQSR_3_STEP.sh)
+if ! echo ${JOBID2} | grep -q "[1-9][0-9]*$"; then 
+   echo "Job(s) submission failed."
+   echo ${JOBID2}
+   exit 1
+else
+   job2=$(echo ${JOBID2} | grep -oh "[1-9][0-9]*$")
+fi
+echo "VQSR of INDELS in progress "
+
+echo "VQSR of INDELS finshed "
+./VQSR_4_SLURM.sh $job2 $1
